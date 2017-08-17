1a15d469a74e52c883c978fad890d0f03a71192d
cpear01 <cpearson_su@yahoo.com>
2017-08-17 14:06:28 -0400

    added variable notes

--- Variables Operators Expressions/Program.cs
+++ Variables Operators Expressions/Program.cs
@@ -10,6 +10,27 @@ namespace Variables_Operators_Expressions
     {
         static void Main(string[] args)
         {
+            char letter = 'A';
+            string name = "Colleen";
+            int age = 20;
+            float money = 2.50f;
+            double rocketCal = 7.8934;
+            decimal number = 3.847258324786m;
+            bool itWorked = true;
+            Console.WriteLine(5 + 15);
+            Console.WriteLine(itWorked);
+            Console.WriteLine(5 < 10);
+
+            Console.WriteLine("enter your first name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName +" " + "Welcome"); //string concatenating between string and integer
+
+
+
+
+
+
+            //initizling a variable - making a memory 
         }
     }
 }
